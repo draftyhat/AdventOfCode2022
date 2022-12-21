4d4d89b4359e85be3935bf3bf08fc2788c369e24
draftyhat <GITEM>
2022-12-21 09:24:12 -0500

    day21 part2: kotlin

--- kotlin/Day21.kt
+++ kotlin/Day21.kt
@@ -28,6 +28,7 @@ fun resolveEquation(firstValue: Long?, operation: String,
     "-" -> firstValue - secondValue
     "*" -> firstValue * secondValue
     "/" -> firstValue / secondValue
+    "=" -> firstValue
     else -> null
   }
 }
@@ -80,6 +81,127 @@ fun readAndSolve(input: String): Long {
   return values["root"]!!
 }
 
+fun addLine(lines: MutableMap<String, MutableList<List<String>>>,
+    lhs: String, rhs: List<String>) {
+  val lineEntry = lines[lhs]
+  if(lineEntry != null)
+    lineEntry.add(rhs)
+  else
+    lines[lhs] = mutableListOf<List<String>>(rhs)
+}
+
+fun addReverseDependencies(
+    lines: MutableMap<String, MutableList<List<String>>>,
+    dependencies: MutableMap<String, MutableList<String>>,
+    lhs: String, equation: List<String>) {
+  val firstOperand = equation[0]
+  val operator = equation[1]
+  val secondOperand = equation[2]
+
+  /* plus converts to minus, multiplication converts to division, etc. */
+  when(operator) {
+    "+" -> {
+      addLine(lines, firstOperand, listOf(lhs, "-", secondOperand))
+      addLine(lines, secondOperand, listOf(lhs, "-", firstOperand))
+    }
+    "-" -> {
+      addLine(lines, firstOperand, listOf(lhs, "+", secondOperand))
+      addLine(lines, secondOperand, listOf(firstOperand, "-", lhs))
+    }
+    "*" -> {
+      addLine(lines, firstOperand, listOf(lhs, "/", secondOperand))
+      addLine(lines, secondOperand, listOf(lhs, "/", firstOperand))
+    }
+    "/" -> {
+      addLine(lines, firstOperand, listOf(lhs, "*", secondOperand))
+      addLine(lines, secondOperand, listOf(firstOperand, "/", lhs))
+    }
+  }
+
+  /* now each of the operands may also be resolved by the lhs and the other
+   * operator */
+  addDependency(dependencies, firstOperand, lhs)
+  addDependency(dependencies, firstOperand, secondOperand)
+  addDependency(dependencies, secondOperand, lhs)
+  addDependency(dependencies, secondOperand, firstOperand)
+}
+
+fun readAndSolve2(input: String): Long {
+  /* starting from the part1 code base, add the part2 twists */
+
+  /* key: monkey, value: key's dependencies */
+  val dependencies = mutableMapOf<String,MutableList<String>>()
+  /* known values */
+  val values = mutableMapOf<String, Long>()
+  /* unparsed lines, keyed by lhs monkey */
+  val lines = mutableMapOf<String, MutableList<List<String>>>()
+  /* values to cascade */
+  val toResolve = mutableListOf<String>()
+
+  /* first attempt to parse each line */
+  for(line in input.trim().split('\n')) {
+     val linesplit = line.split(": ")
+     if(linesplit[0] == "humn")
+       continue
+     try {
+      values[linesplit[0]] = linesplit[1].toLong()
+      toResolve.add(linesplit[0])
+     } catch(e: Exception) {
+       /* not a value line. Put it in unparsed. Also, parse out dependencies
+          for this monkey. */
+       val depsplit = linesplit[1].split(' ')
+       if(linesplit[0] == "root") {
+         /* root is special */
+         addDependency(dependencies, depsplit[0], depsplit[2])
+         addDependency(dependencies, depsplit[2], depsplit[0])
+         /* add a fake equation: value1: value2 = value2
+            which resolveEquation will know how to deal with */
+         addLine(lines, depsplit[0],
+             listOf<String>(depsplit[2], "=", depsplit[2]))
+         addLine(lines, depsplit[2], listOf<String>(depsplit[0], "=",
+             depsplit[0]))
+       } else {
+         addDependency(dependencies, linesplit[0], depsplit[0])
+         addDependency(dependencies, linesplit[0], depsplit[2])
+         addLine(lines, linesplit[0], depsplit)
+         /* now because we may be working backwards, we add equations
+            that will resolve any one of the operands too */
+         addReverseDependencies(lines, dependencies, linesplit[0], depsplit)
+       }
+     }
+  }
+
+  /* now resolve dependencies until we're done */
+  while(toResolve.size > 0) {
+    val monkey = toResolve.removeAt(0)
+    dependencies[monkey]?.forEach { depender ->
+      if(values[depender] == null) {
+        /* check to see if this depender is completely resolved */
+        val equations = lines[depender]!!
+        var solved = false
+        for(equation in equations) {
+          val equationValue = resolveEquation(values[equation[0]],
+              equation[1], values[equation[2]])
+          if(equationValue != null) {
+            values[depender] = equationValue
+            solved = true
+            break
+          } else {
+            /* else couldn't resolve this monkey, add it to the end of the list to
+             * try later */
+          }
+        }
+        if(solved)
+          toResolve.add(monkey)
+        else
+          toResolve.add(depender)
+      }
+    }
+  }
+
+  return values["humn"]!!
+}
+
 fun Part1(input:String) : Boolean {
   val answer = readAndSolve(input)
   println("Root yells $answer!")
@@ -87,7 +209,9 @@ fun Part1(input:String) : Boolean {
 }
 
 fun Part2(input:String) : Boolean {
-  return false
+  val answer = readAndSolve2(input)
+  println("humn yells $answer!")
+  return true
 }
 
 fun main(args: Array<String>) {
